eef55b5a75004d928156e1748d5607ce51e167b2
santanam23 <mirmir11.ms@gmail.com>
2022-01-26 15:02:11 -0600

    created an index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const generatePage = () => 'Name: Miriam, Github: santanam23';
+
+console.log(generatePage());
