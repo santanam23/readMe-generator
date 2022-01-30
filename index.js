85c892df2f6fe48452edeec1f965ed36efc27bfd
santanam23 <mirmir11.ms@gmail.com>
2022-01-30 10:48:46 -0600

    adding license

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer'); 
 
 // linking to page where the README is developed 
-const generatePage = require('./utils/generatePage.js');
+const generatePage = require('./utils/generateMarkdown.js');
 
 // array of questions for user
 const questions = () => {
