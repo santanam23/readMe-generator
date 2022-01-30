bbe12fcf9afbfaecbb4e0b572f4b1ef0bfe8ed41
santanam23 <mirmir11.ms@gmail.com>
2022-01-30 10:26:44 -0600

    created a gitignore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer'); 
 
 // linking to page where the README is developed 
-const generatePage = require('./utils/generateReadme.js');
+const generatePage = require('./utils/generatePage.js');
 
 // array of questions for user
 const questions = () => {
